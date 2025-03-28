05f7de1876f6b8ec1c11b1476c3353f2cd72e161
Mofakkharul Islam <mofakkharulislam29@gmail.com>
2025-03-29 00:26:35 +0600

    vehicle is updated

--- vehicle.dart
+++ vehicle.dart
@@ -1,1 +1,25 @@
+int main() {
+  Car car = Car(25);
+  car.move();
 
+  return 0;
+}
+
+abstract class Vehicle {
+  int? _speed;
+  Vehicle(this._speed);
+
+  void move();
+
+  // set setSpeed(int speed) {
+  //   this._speed = speed;
+  // }
+}
+
+class Car extends Vehicle {
+  Car(int? speed) : super(speed);
+  @override
+  void move() {
+    print("The car is moving at ${super._speed} km/h");
+  }
+}
